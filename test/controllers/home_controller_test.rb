66b9c1a96f8a8b47d644577c5ad2621f859aeb68
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2025-06-24 00:18:23 -0300

    test: :test_tube: fix wrong test devise user

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
     user = FactoryBot.create(:user)
     sign_in user
     get root_path
