3eef47deb0661ef8bdd2f87e72bc9ef3228ee5ca
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2025-06-24 00:21:30 -0300

    test: :test_tube: try to fix home controller test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
     user = FactoryBot.create(:user)
+
+    get new_user_session_path
+
     sign_in user
     get root_path
     assert_response :success
