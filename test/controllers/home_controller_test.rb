1a9f2eb9032e097b0e230b3529c7f93e17869bb5
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2025-06-24 00:24:59 -0300

    test: :test_tube: try to fix home controller test user

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,10 +3,13 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     user = FactoryBot.create(:user)
-
     get new_user_session_path
-
     sign_in user
+
+    # stub do helper
+    Rails.application.routes.default_url_options[:host] = "localhost:3000"
+    self.class.include Devise::OmniAuth::UrlHelpers rescue nil
+
     get root_path
     assert_response :success
   end
