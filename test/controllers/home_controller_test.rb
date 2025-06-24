e75f00b483f0804f4e1a34ab7530a7850404088e
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2025-06-24 00:30:12 -0300

    test: :white_check_mark: cchange home test to a most value test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,15 +2,8 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    user = FactoryBot.create(:user)
-    get new_user_session_path
-    sign_in user
-
-    # stub do helper
-    Rails.application.routes.default_url_options[:host] = "localhost:3000"
-    self.class.include Devise::OmniAuth::UrlHelpers rescue nil
-
     get root_path
     assert_response :success
+    assert_select "h1", text: "Bem-vindo" # exemplo: testa se tem um h1 com "Bem-vindo"
   end
 end
